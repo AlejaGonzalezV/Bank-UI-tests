f11bb924e6e5be3ce1a59d19365e4243c1994941
OPOII <41926039+OPOII@users.noreply.github.com>
2021-05-24 15:19:12 -0500

    Update update-user.spec.js

---
+++ cypress/integration/update-user.spec.js
@@ -0,0 +1,37 @@
+const { describe } = require("mocha");
+
+const completeName="Pruebita de update";
+var cedula="";
+const usuario="Ya no me voa matar, wiii";
+var estado="";
+
+describe('When the user wants to add a user',()=>{
+    before(()=>{
+        cy.visit('https://bank-users-ui.herokuapp.com/');
+        cy.get(':nth-child(2) > a').click();
+        cy.get(':nth-child(1) > :nth-child(5) > .MuiGrid-container > :nth-child(1) > .MuiButtonBase-root').click();
+        cy.get('#standard-basic').click();
+        cy.get('#standard-basic').clear();
+        cy.get('#standard-basic').type(completeName);
+        cy.get('#identification').then(($lab)=>{
+        cedula=$lab.text();
+        })
+        console.log(cedula);
+        cy.get('#username').click();
+        cy.get('#username').clear();
+        cy.get('#username').type(usuario);
+        cy.get('#stateA').click();
+        cy.get('.MuiList-root > [tabindex="-1"]').click();
+        cy.get('#stateA').invoke('text').then((text)=>{
+        estado=text;
+        });
+        cy.get('.MuiButtonBase-root').first().click();
+    });
+    it("then, the user should be listed with the rigth name, cedula, usuario and estado",()=>{
+        cy.get('.MuiTableBody-root').contains(':nth-child(1)',completeName).should('be.visible');
+       // cy.get('.MuiTableBody-root').contains('td',completeName).should('be.visible');
+        cy.get('.MuiTableBody-root').contains(':nth-child(2)',cedula).should('be.visible');
+        cy.get('.MuiTableBody-root').contains(':nth-child(3)',usuario).should('be.visible');
+        cy.get('.MuiTableBody-root').contains(':nth-child(4)',estado).should('be.visible');
+    });
+});
