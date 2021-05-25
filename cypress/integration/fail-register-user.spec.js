c3e920338ad58008225b1296887d6cd80e00b661
OPOII <41926039+OPOII@users.noreply.github.com>
2021-05-25 17:09:40 -0500

    Update fail-register-user.spec.js

--- cypress/integration/fail-register-user.spec.js
+++ cypress/integration/fail-register-user.spec.js
@@ -17,3 +17,69 @@ describe('When the user wants to add a person withouth the name',()=>{
         cy.get('.MuiButtonBase-root').should('be.disabled');
     });
 });
+
+describe('When the user wants to add a person withouth the name',()=>{
+    before(()=>{
+        cy.visit('https://bank-users-ui.herokuapp.com/');
+        cy.get(':nth-child(3) > a').click();
+        cy.get('#standard-basic').click();
+        cy.get('#standard-basic').type(completeName);
+        cy.get('#username').click();
+        cy.get('#username').type(usuario);
+        cy.get('#stateA').click();
+        cy.get('.MuiList-root > [tabindex="0"]').click();
+    });
+    it("the button to save must not be enable",()=>{
+        cy.get('.MuiButtonBase-root').should('be.disabled');
+    });
+});
+
+describe('When the user wants to add a person withouth the name',()=>{
+    before(()=>{
+        cy.visit('https://bank-users-ui.herokuapp.com/');
+        cy.get(':nth-child(3) > a').click();
+        cy.get('#standard-basic').click();
+        cy.get('#standard-basic').type(completeName);
+        cy.get('#identification').click();
+        cy.get('#identification').type(cedula);
+        cy.get('#stateA').click();
+        cy.get('.MuiList-root > [tabindex="0"]').click();
+    });
+    it("the button to save must not be enable",()=>{
+        cy.get('.MuiButtonBase-root').should('be.disabled');
+    });
+});
+
+describe('When the user wants to add a person withouth the name',()=>{
+    before(()=>{
+        cy.visit('https://bank-users-ui.herokuapp.com/');
+        cy.get(':nth-child(3) > a').click();
+        cy.get('#standard-basic').click();
+        cy.get('#standard-basic').type(completeName);
+        cy.get('#identification').click();
+        cy.get('#identification').type(cedula);
+        cy.get('#username').click();
+        cy.get('#username').type(usuario);
+    });
+    it("the button to save must not be enable",()=>{
+        cy.get('.MuiButtonBase-root').should('be.disabled');
+    });
+});
+
+describe('When the user wants to add a person withouth the name',()=>{
+    before(()=>{
+        cy.visit('https://bank-users-ui.herokuapp.com/');
+        cy.get(':nth-child(3) > a').click();
+        cy.get('#standard-basic').click();
+        cy.get('#standard-basic').type(completeName);
+        cy.get('#identification').click();
+        cy.get('#identification').type(cedula);
+        cy.get('#username').click();
+        cy.get('#username').type(usuario);
+        cy.get('.MuiList-root > [tabindex="0"]').click();
+        cy.get('.MuiButtonBase-root').first().click();
+    });
+    it("the button to save must not be enable",()=>{
+        
+    });
+});
