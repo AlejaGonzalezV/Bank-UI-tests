62e9d531fbf8d2a53f4e68a259ac74fe5bfc481b
OPOII <41926039+OPOII@users.noreply.github.com>
2021-05-25 16:45:57 -0500

    finally finished this shit

---
+++ cypress/integration/fail-register-user.spec.js
@@ -0,0 +1,19 @@
+const completeName="Juan Carlos Xamarin";
+const cedula="118855997";
+const usuario="Me voa matar, wiiiii";
+const estado="Activo";
+
+describe('When the user wants to add a person withouth the name',()=>{
+    before(()=>{
+        cy.visit('https://bank-users-ui.herokuapp.com/');
+        cy.get(':nth-child(3) > a').click();
+        cy.get('#identification').type(cedula);
+        cy.get('#username').click();
+        cy.get('#username').type(usuario);
+        cy.get('#stateA').click();
+        cy.get('.MuiList-root > [tabindex="0"]').click();
+    });
+    it("the button to save must not be enable",()=>{
+        cy.get('.MuiButtonBase-root').should('be.disabled');
+    });
+});
